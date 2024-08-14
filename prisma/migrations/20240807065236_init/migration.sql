a66113d391238ee6cef605ba159311a1608d0ff5
sar4th <sarathrajklm0475@gmail.com>
2024-08-14 19:33:47 +0530

    fix: db deleted

--- prisma/migrations/20240807065236_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "PoolDetails" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "poolAmount" TEXT NOT NULL,
-    "monthyPayment" TEXT NOT NULL,
-    "totalWinners" TEXT NOT NULL,
-    "activeUsers" TEXT NOT NULL
-);
